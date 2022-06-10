70a1a0b324ce3c19abbb1ed37506c1f247018047
BlairDesjardins <blairmichaeldesjardins@gmail.com>
2022-06-10 13:49:29 -0400

    checkout functions working!

--- js/main.js
+++ js/main.js
@@ -1,60 +1,94 @@
 async function checkoutCart() {
+    const cart = 
+    [
+        {
+            p_id: 44,
+            name: "HDMI Cable Dual Package",
+            desc: "A package of two HDMI cables",
+            price: 12.0,
+            stock: 100
+        },
+        {
+            p_id: 45,
+            name: "Cat Ear Headphones",
+            desc: "Headphones with cat ears",
+            price: 60.0,
+            stock: 100
+        }
+    ]
+
+    const user = {
+        uId: 2,
+        email: "JaneSmith@aol.com",
+        firstName: "Jane",
+        lastName: "Smith"
+    }
+
+    sessionStorage.setItem('cart', JSON.stringify(cart))
+    sessionStorage.setItem('user', JSON.stringify(user))
     parseOrder()
     checkoutParse()
 }
 async function parseOrder() {
-    orderList = sessionStorage.getItem('');
+    orderList = JSON.parse(sessionStorage.getItem('cart'));
+
+    document.getElementById("cartNum").innerHTML = orderList.length
+    let total = 0;
     
     orderList.forEach(element => {
         const outerElement = document.createElement('tr');
         const innerElement = document.createElement('td');
         const elmnt = document.createElement('a');
 
-
-
-        innerElement.setAttribute('class', 'table m-y');
         elmnt.setAttribute('id', 'cartItems');
         elmnt.setAttribute('href', '');
+        elmnt.innerHTML = element.name
+
+        const price = document.createElement('span')
+        price.innerHTML = `$${element.price}`
+
+        total += element.price
 
         const cart = document.getElementById('userCart');
         cart.appendChild(outerElement);
         outerElement.appendChild(innerElement);
         innerElement.appendChild(elmnt);
-        elmnt.appendChild(quant);
-        
+        innerElement.appendChild(price);
+        //elmnt.appendChild(quant);
     });
+    
+    document.getElementById("totalPrice").innerHTML = `$${total}`
 }
 
 async function checkoutParse() {
-    const user = sessionStorage.getItem('');
+    const user = JSON.parse(sessionStorage.getItem('user'));
     if (user) {
-        document.getElementById('fName').innerHTML = user.firstName;
-        document.getElementById('lName').innerHTML = user.lastName;
-        document.getElementById('email').innerHTML = user.email;
+        document.getElementById('fName').value = user.firstName;
+        document.getElementById('lName').value = user.lastName;
+        document.getElementById('email').value = user.email;
         document.getElementById('fName').disabled = true;
         document.getElementById('lName').disabled = true;
         document.getElementById('email').disabled = true;
-
         
-        const userId = user.userId;
+        const userId = user.uId;
         const httpurl = `http://localhost:7000/address/user/${userId}`;
         const httpResponse = await fetch(httpurl);
-        const address = await httpResponse.json();
-        sessionStorage.setItem('userAddress', address)
-        if (address && address.length > 0) {
-            document.getElementById('adr').innerHTML = address.address;
-            document.getElementById('city').innerHTML = address.city;
-            document.getElementById('pCode').innerHTML = address.postal_code;
-            document.getElementById('ctr').innerHTML = address.country;
+        const address = (await httpResponse.json())[0];
+        sessionStorage.setItem('userAddress', JSON.stringify(address))
+        if (address) {
+            document.getElementById('adr').value = address.address;
+            document.getElementById('city').value = address.city;
+            document.getElementById('pCode').value = address.postal_code;
+            document.getElementById('ctr').value = address.country;
         }          
     }
 }
 
 async function checkout() {
-    const address = sessionStorage.getItem('userAddress');
-    const user = sessionStorage.getItem('');
-    const orderList = sessionStorage.getItem('')
-    if (!address || address.length == 0) {
+    const address = JSON.parse(sessionStorage.getItem('userAddress'));
+    const user = JSON.parse(sessionStorage.getItem('user'));
+    const orderList = JSON.parse(sessionStorage.getItem('cart'))
+    if (!address) {
         const userAddressData = {
             u_id: user.userId,
             address: document.getElementById('adr').innerHTML,
@@ -74,8 +108,8 @@ async function checkout() {
     };
 
     const orderData = {
-        uID: 2,
-        dateOfPurchase: new Date().getTime()*1000,
+        uID: user.uId,
+        dateOfPurchase: (new Date().getTime()*1000),
         aID: address.a_id
     };
     const orderOption = {
@@ -84,13 +118,13 @@ async function checkout() {
         body: JSON.stringify(orderData)
     };
 
-    const orderResponse = await fetch("http://localhost:7000/order/", orderOption);
+    const orderResponse = await fetch("http://localhost:7000/orders/", orderOption);
     const newOrder = await orderResponse.json();
 
-    orderList.forEach(element=>{
+    for (element of orderList) {
         const orderDetailData =  {
             oID: newOrder.oID,
-            pID: 44,
+            pID: element.p_id,
             quantity: 1
         };
 
@@ -99,15 +133,10 @@ async function checkout() {
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify(orderDetailData)
         };
-        const orderDetailResponse = await fetch('http://localhost:7000//orderDetails/',orderDetailOption);
+        const orderDetailResponse = await fetch('http://localhost:7000/orderDetails/',orderDetailOption);
         const newOrderDetail = await orderDetailResponse.json()
-    })
+    }
     console.log('Order Successful');
           
     
-
-  
-
-    
-
 }
